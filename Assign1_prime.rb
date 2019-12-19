845a75f405081a7b68734c8ae546cff26d82a6a9
DevKaranPradhan <dvpradhan312@gmail.com>
2019-12-19 13:16:18 +0530

    program to check if a number is prime

---
+++ Assign1_prime.rb
@@ -0,0 +1,24 @@
+print "enter a number "
+num = gets.chomp.to_i
+
+ct=0
+
+if(num<2)
+	puts "#{num} is not prime"
+
+else
+	i=2
+	while(i<=num/2)
+		if(num%i==0) 
+			ct+=1
+			break
+		end
+		i+=1
+	end
+end
+
+if(ct>=1) 
+	puts "#{num} is not prime"
+else
+	puts "#{num} is prime"
+ends
